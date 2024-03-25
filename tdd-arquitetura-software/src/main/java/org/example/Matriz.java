2ae2aacf07cc380c83bd645bd9a9d6b11a9402d3
jzpacheco <jeffersonpacheco@hotmail.com>
2024-03-25 16:49:16 -0300

    feat: created raw methods that will be tested

--- tdd-arquitetura-software/src/main/java/org/example/Matriz.java
+++ tdd-arquitetura-software/src/main/java/org/example/Matriz.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class Matriz {
+
+    public static int[][] somaMatrizes(int[][] matriz1, int[][] matriz2){
+      return null;
+    };
+    public static int[][] subtracaoMatrizes(int[][] matriz1, int[][] matriz2){
+        return null;
+    };
+    public static int[][] multiplicacaoMatrizes(int[][] matriz1, int[][] matriz2){
+        return null;
+    };
+    public static int[][] multiplicacaoPorEscalar(int[][] matriz1, int[][] matriz2){
+        return null;
+    };
+    public static int[][] transposicaoMatriz(int[][] matriz1, int[][] matriz2){
+        return null;
+    };
 }

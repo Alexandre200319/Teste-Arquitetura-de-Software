f21bcdac1ebf86ab09c2d67cf9f9affa9659c748
jzpacheco <jeffersonpacheco@hotmail.com>
2024-03-25 16:52:06 -0300

    feat: created method for sum

--- tdd-arquitetura-software/src/main/java/org/example/Matriz.java
+++ tdd-arquitetura-software/src/main/java/org/example/Matriz.java
@@ -3,7 +3,24 @@ package org.example;
 public class Matriz {
 
     public static int[][] somaMatrizes(int[][] matriz1, int[][] matriz2){
-      return null;
+        if (matriz1.length != matriz2.length || matriz1[0].length != matriz2[0].length) {
+            // Se as dimensões das matrizes forem diferentes, retorna null
+            System.out.println("As matrizes têm dimensões diferentes.");
+            return null;
+        }
+
+        int linhas = matriz1.length;
+        int colunas = matriz1[0].length;
+
+        int[][] resultado = new int[linhas][colunas];
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+
+        return resultado;
     };
     public static int[][] subtracaoMatrizes(int[][] matriz1, int[][] matriz2){
         return null;
